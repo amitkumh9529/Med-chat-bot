1ac1d273429cc98cd665aea1bbae24ed7b7f8398
Amit Halder <amith3169@gmail.com>
2026-03-23 22:48:03 +0530

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="med_chat_bot",
+    version="0.0.1",
+    author="Amith",
+    author_email="amith3169@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
---
+++ src/helper.py
@@ -0,0 +1,64 @@
+from langchain.document_loaders import PyPDFLoader,DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from typing import List
+from langchain.schema import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+
+#Extract Text from PDF
+
+def load_pdf_files(data):
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+    document=loader.load()
+    return document
+
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    """
+    Given a list of Document objects, return a new list of Document objects
+    containing only 'source' in metadata and the original page_content.
+    """
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata={"source": src}
+            )
+        )
+    return minimal_docs
+
+
+#split the document into chunks
+
+def text_split(minimal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,
+        
+    )
+    text_chunk = text_splitter.split_documents(minimal_docs)
+    return text_chunk
+
+# texts_chunk = text_split(minimal_docs)
+
+
+#Download Embeddings
+
+def download_embeddings():
+    """
+    Download and return the HuggingFace embeddings model.
+    """
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name
+    )
+    return embeddings
+
+# embedding = download_embeddings()
